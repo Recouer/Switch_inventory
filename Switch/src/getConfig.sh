3a699607d620b4d635c3e35abbe6347ca026dba6
MANNONE Guillaume <Guillaume.Mannone@enseirb-matmeca.fr>
2020-08-10 10:00:26 +0200

    working version using ssh and telnet

--- Switch/src/getConfig.sh
+++ Switch/src/getConfig.sh
@@ -8,6 +8,7 @@ PROTOCOL=$4
 DATA=./SwitchData/tmp/temp.txt
 
 ./src/getData.sh $SERVER $USER $PASS $PROTOCOL "Show version" > $DATA
+./src/getData.sh $SERVER $USER $PASS $PROTOCOL "Show version" > $DATA
 
 get_switch_data() {
     switchName=$1
@@ -20,11 +21,26 @@ get_switch_data() {
     fi
 }
 
+####    Add Here    ####
+get_switch_data "WS-C2960-24PC-L" "24" "Fa0/" "6"
+get_switch_data "WS-C3560-24PS" "24" "Fa0/" "6"
 get_switch_data "C9300-24" "24" "Gi1/0/" "9"
-echo "$portsNb" "$portsName" "$portsNameSize"
 
 if [[ -z $portsNb ]]
 then
-    echo "Switch not Registered"
-    exit
+    cat $DATA
+    ./src/getData.sh $SERVER $USER $PASS $PROTOCOL "Show interface status"
+    echo ""
+    
+    read -p "switchName:" switchName
+    read -p "portsNb:" portsNb
+    read -p "portsName:" portsName
+    portsNameSize=$(expr ${#portsName} + 2)
+
+    sed -i "23 a get_switch_data \"$switchName\" \"$portsNb\" \"$portsName\" \"$portsNameSize\"" ./src/getConfig.sh
+fi
+
+if [[ -z $5 ]]
+then
+    echo "$switchName" "$portsNb" "$portsName" "$portsNameSize"
 fi
